f5a4815951877619d9a1bb383fb75b8073eceee7
hibby <hibaqku7@gmail.com>
2025-04-19 22:12:47 +0300

    pass test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,8 @@
 // Your tests here
+import { pointsForWord } from "../utils";
+
+describe("pointsForWord", () => {
+  it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
+
+  });
+});
